be74a3ed6e63bc551cd8aeaa10b205d9fe3d5643
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2020-06-07 19:34:10 -0300

    feat: adding new scripts and exporting functions in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+const context = path.join(__dirname, 'src');
+
 module.exports = {
+  context,
+  entry: './index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    library: 'finalSpaceCharacters',
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
